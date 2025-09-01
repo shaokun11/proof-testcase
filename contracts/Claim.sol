278d4aad67922714e0fdd14d363a195df6759223
shaokun <605416017@qq.com>
2025-09-01 21:43:58 +0800

    init

---
+++ contracts/Claim.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleClaim {
+    bytes32 public merkleRoot;
+
+    mapping(address => uint256) public claimed;
+
+    event MerkleRootUpdated(bytes32 merkleRoot);
+    event Claimed(address indexed account, uint256 index, uint256 claimCount);
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) external {
+        merkleRoot = _merkleRoot;
+        emit MerkleRootUpdated(_merkleRoot);
+    }
+
+    function claim(uint256 index, bytes32[] calldata merkleProof) external {
+        uint i = 0 ;
+        while(i < 100) {
+            bytes32 leaf = keccak256(
+                        abi.encodePacked(msg.sender, index)
+                    );
+            require(
+                MerkleProof.verify(merkleProof, merkleRoot, leaf),
+                "Invalid proof"
+            );
+            i++;
+        }
+        claimed[msg.sender] += 1;
+
+        emit Claimed(msg.sender, index, claimed[msg.sender]);
+    }
+}
---
+++ scripts/start.ts
@@ -0,0 +1,96 @@
+import "dotenv/config";
+import { MerkleTree } from "merkletreejs";
+import keccak256 from "keccak256";
+import { network } from "hardhat";
+
+
+const net = process.env.NETWORK || "hardhat";
+const conflictRate = parseFloat(process.env.CONFLICT_RATE || "0");
+const txCount = parseInt(process.env.TX_COUNT || "10");
+
+const { ethers } = await network.connect({
+    network: net,
+});
+
+const [sender] = await ethers.getSigners();
+interface Claim {
+    index: number;
+    account: string;
+}
+
+async function faucet(arr: string[], amount: string) {
+    const bal = await sender.provider!.getBalance(sender.address);
+    console.log("sender balance is:", ethers.formatEther(bal));
+    const nonce = await sender.getNonce();
+    console.log("start faucet");
+    for (let i = 0; i < arr.length; i++) {
+        const tx = await sender.sendTransaction({
+            to: arr[i],
+            value: ethers.parseEther(amount),
+            nonce: nonce + i,
+        });
+        if (i + 1 === arr.length) {
+            await tx.wait();
+        }
+    }
+    console.log("faucet end");
+}
+
+async function main() {
+    const n = Math.floor(txCount * (1 - conflictRate));
+    const amount = "0.1";
+    const provider = ethers.provider!;
+
+    console.log("create %d wallets", n);
+    const wallets = Array.from({ length: n }, () =>
+        ethers.Wallet.createRandom().connect(provider)
+    );
+
+    await faucet(wallets.map((w) => w.address), amount);
+    const claims: Claim[] = wallets.map((w, index) => {
+        return {
+            index,
+            account: w.address,
+        };
+    });
+    const leaves = claims.map((c) =>
+        keccak256(
+            ethers.solidityPacked(["address", "uint256"], [c.account, c.index])
+        )
+    );
+    const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+    const root = tree.getHexRoot();
+    console.log("Merkle Root:", root);
+
+    const MerkleClaim = await ethers.getContractFactory("MerkleClaim");
+    const mc = await MerkleClaim.deploy(root);
+    await mc.waitForDeployment();
+    console.log("deploy proof contract address is:", await mc.getAddress());
+    let block = await sender.provider!.getBlockNumber();
+    console.log("send tx previous block number is:", block);
+
+    // no conflict tx
+    for (let i = 0; i < wallets.length; i++) {
+        const c = claims[i];
+        const leaf = keccak256(
+            ethers.solidityPacked(["address", "uint256"], [c.account, c.index])
+        );
+        const proof = tree.getHexProof(leaf);
+        const mcWithWallet = mc.connect(wallets[i]);
+        let startNonce = await wallets[i].getNonce();
+        for (let j = 0; j < 100; j++) {
+            let tx = await mcWithWallet.claim(c.index, proof,{nonce: startNonce + j});
+            if (i + 1 === wallets.length && j + 1 === 100) {
+                await tx.wait();
+            }
+        }
+
+    }
+    block = await sender.provider!.getBlockNumber();
+    console.log("claim block number is:", block);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
